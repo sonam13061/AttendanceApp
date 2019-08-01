f7584fe1df7f109b784b883408352e79c140b0e3
sonam13061 <sonamkhurana589@gmail.com>
2019-08-01 14:37:44 +0530

    Forgot password+email verification

--- app/src/main/java/com/example/attendanceapp/SplashActivity.java
+++ app/src/main/java/com/example/attendanceapp/SplashActivity.java
@@ -3,10 +3,18 @@ package com.example.attendanceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SplashActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -16,15 +24,16 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         mAuth = FirebaseAuth.getInstance();
+        hash();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(mAuth.getCurrentUser()==null){
-                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
-                startActivity(intent);
-                finish();}
-                else {
-                    Intent intent=new Intent(SplashActivity.this,HomeActivity.class);
+                if (mAuth.getCurrentUser() == null) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
                     startActivity(intent);
                     finish();
 
@@ -33,4 +42,30 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 3000);
     }
+
+
+
+    public  void hash() {
+
+     try
+
+    {
+        PackageInfo info = getPackageManager().getPackageInfo("com.example.attendanceapp", PackageManager.GET_SIGNATURES);
+        for (Signature signature : info.signatures) {
+            MessageDigest md = MessageDigest.getInstance("SHA");
+            md.update(signature.toByteArray());
+            Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+        }
+    } catch(
+    PackageManager.NameNotFoundException e)
+
+    {
+        Log.d("errorrr", e.getMessage());
+    } catch(
+    NoSuchAlgorithmException e)
+
+    {
+        Log.d("errorrr", e.getMessage());
+    }
+}
 }
